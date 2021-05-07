4c6d315cde7cb8f9d5e923667b9ebab13600e349
Esouder <37464714+Esouder@users.noreply.github.com>
2021-05-07 16:56:49 -0700

    Mostly finished bot code

--- twitter.py
+++ twitter.py
@@ -1,14 +1,28 @@
+
+# for package management. I want to be able to use this with AWS Lambda so I 
+# thought it would be easier if all the packages were right here
 import os
 import sys
 
-# Add vendor directory to module search path
+# this is a txt file on my website where the random lines to be tweeted come from.
+data_url = 'https://www.souder.ca/TweetableText.txt'
+
+# Add vendor directory to module search path - for external modules
 parent_dir = os.path.abspath(os.path.dirname(__file__))
 vendor_dir = os.path.join(parent_dir, 'vendor')
 
 sys.path.append(vendor_dir)
 
+# imports
 from twython import Twython
+from twython import TwythonError
+import urllib.request
+import random
 
+# for randomness
+random.seed()
+
+# get twitter auth stuff
 from auth import (
     API_key,
     API_secret,
@@ -16,6 +30,7 @@ from auth import (
     access_token_secret
 )
 
+# twitter API setup
 twitter = Twython(
     API_key,
     API_secret,
@@ -23,6 +38,33 @@ twitter = Twython(
     access_token_secret
 )
 
-message = 'This was tweeted from a python script using the Twitter API!'
-twitter.update_status(status=message)
-print("Tweeted: %s" %message)
+# file manipulation
+data = urllib.request.urlopen(data_url).read(2000).decode("utf-8").split("\n")
+num_lines = len(data)-1
+
+success=False
+
+pickedLine = random.randint(0, num_lines-1)
+print(pickedLine)
+
+
+#while(success==False):
+try:
+    # create message
+    message =data[pickedLine]
+
+    # send message
+    twitter.update_status(status=message)
+
+    #success=True
+except TwythonError:
+    pickedLine += 1
+    if(pickedLine >= num_lines):
+        pickedLine=0
+    print(pickedLine)
+    
+    # create message
+    message =data[pickedLine]
+
+    # send message
+    twitter.update_status(status=message)
